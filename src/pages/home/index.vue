ac31a03bce0d0fe7fe64ec4ba0e5cbfb00697687
小帅 <2944435683>
2025-02-10 14:31:05 +0800

    no message

--- src/pages/home/index.vue
+++ src/pages/home/index.vue
@@ -1,40 +1,49 @@
 <template>
   <div class="home">
-    <div class="data" v-for="(item, index) in tableData" :key="index">
-      <div class="jb">
-        <div>
-          {{ item.class }}
-        </div>
-        <div class="addClass">
-          <el-button type="primary" @click="add(item)" round>添加物料</el-button>
-        </div>
+    <div class="jb">
+      <div></div>
+      <div>
+        <el-button type="primary" @click="reduction">一键还原数据</el-button>
       </div>
-      <div class="dataBox">
-        <div class="jb" v-for="(items, indexs) in item.data" :key="indexs">
-          <div class="materials">
-            <el-descriptions :title="items.name">
-              <div v-for="(itemS, indexS) in items.children" :key="indexS">
-                <div>
-                  <el-descriptions-item label="物料名称">
-                    {{ itemS.name }}
-                  </el-descriptions-item>
-                  <el-descriptions-item label="物料效期">
-                    {{ itemS.theTerm }}
-                  </el-descriptions-item>
-                  <el-descriptions-item label="存储方式">
-                    {{ itemS.storage }}
-                  </el-descriptions-item>
-                </div>
-              </div>
-            </el-descriptions>
+    </div>
+    <div class="list">
+      <div class="data" v-for="(item, index) in tableData" :key="index">
+        <div class="jb">
+          <div>
+            {{ item.class }}
           </div>
-          <div class="btn">
-            <el-button type="warning" size="small" round @click="editMaterials(items)">编辑物料</el-button>
-            <el-button type="danger" size="small" round @click="delMaterials(items)">删除物料</el-button>
+          <div class="addClass">
+            <el-button type="primary" @click="add(item)" round>添加物料</el-button>
+          </div>
+        </div>
+        <div class="dataBox">
+          <div class="jb" v-for="(items, indexs) in item.data" :key="indexs">
+            <div class="materials">
+              <el-descriptions :title="items.name">
+                <div v-for="(itemS, indexS) in items.children" :key="indexS">
+                  <div>
+                    <el-descriptions-item label="物料名称">
+                      {{ itemS.name }}
+                    </el-descriptions-item>
+                    <el-descriptions-item label="物料效期">
+                      {{ itemS.theTerm }}
+                    </el-descriptions-item>
+                    <el-descriptions-item label="存储方式">
+                      {{ itemS.storage }}
+                    </el-descriptions-item>
+                  </div>
+                </div>
+              </el-descriptions>
+            </div>
+            <div class="btn">
+              <el-button type="warning" size="small" round @click="editMaterials(items)">编辑物料</el-button>
+              <el-button type="danger" size="small" round @click="delMaterials(items)">删除物料</el-button>
+            </div>
           </div>
         </div>
       </div>
     </div>
+
     <newHome v-model="homeShow" />
     <newEdit v-model="editShow" />
   </div>
@@ -77,7 +86,10 @@ function delMaterials(items) {
   }).then(() => {
     tableData.value.forEach((category) => {
       category.data = category.data.reduce((acc, item) => {
-        item.children = item.children.filter((child) => !items.children.some((delItem) => delItem.name === child.name));
+        item.children = item.children.filter(
+          (child) =>
+            !items.children.some((delItem) => delItem.name === child.name && delItem.storage === child.storage && delItem.theTerm === child.theTerm)
+        );
         if (item.children.length > 0) {
           acc.push(item);
         }
@@ -97,32 +109,55 @@ function delMaterials(items) {
       .catch(() => stop());
   });
 }
+//还原数据
+function reduction() {
+  ElMessageBox.confirm("是否确定还原数据", {
+    confirmButtonText: "还原",
+    cancelButtonText: "取消",
+    type: "warning",
+  }).then(async () => {
+    const { data } = await $axios.get("/printList/getPrintList");
+    const stop = $dialogLoading();
+    $axios
+      .post("/printData/addPrintData", {
+        data: data,
+      })
+      .then(({ data }) => {
+        stop();
+        ElMessage.success("还原成功");
+        home().getPrintData();
+      })
+      .catch(() => stop());
+  });
+}
 </script>
 
 <style lang="scss" scoped>
 .home {
-  display: grid;
-  grid-template-columns: repeat(2, 1fr);
-  gap: 20px;
-  .data {
-    padding: 10px;
-    background-color: rgb(218, 209, 209);
-    border-radius: 10px;
-    margin-top: 20px;
-    .addClass {
-    }
-    .dataBox {
-      background-color: #ffffff;
-      margin-top: 5px;
-      height: 400px;
+  .list {
+    display: grid;
+    grid-template-columns: repeat(2, 1fr);
+    gap: 20px;
+    .data {
+      padding: 10px;
+      background-color: rgb(218, 209, 209);
       border-radius: 10px;
-      overflow: auto;
-      padding: 5px;
-      .materials {
-        width: 85%;
+      margin-top: 20px;
+      .addClass {
       }
-      .btn {
-        width: 15%;
+      .dataBox {
+        background-color: #ffffff;
+        margin-top: 5px;
+        height: 400px;
+        border-radius: 10px;
+        overflow: auto;
+        padding: 5px;
+        .materials {
+          width: 85%;
+        }
+        .btn {
+          width: 15%;
+        }
       }
     }
   }
