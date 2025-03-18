4a0e667733d376ccb155b0bef965e6423902001d
小帅 <2944435683>
2025-03-18 14:20:36 +0800

    no message

--- src/pages/account/brand/index.vue
+++ src/pages/account/brand/index.vue
@@ -1,9 +1,41 @@
 <template>
-  <div class="brand">2323</div>
+  <div class="account">
+    <vxe-table :column-config="{ resizable: true }" border ref="tableRef" :data="tableData">
+      <vxe-column type="seq" width="70"></vxe-column>
+      <vxe-column field="username" title="账号"></vxe-column>
+      <vxe-column field="name" title="名称"></vxe-column>
+      <vxe-column field="code" title="店铺编码"></vxe-column>
+      <vxe-column field="storeName" title="店铺名称"></vxe-column>
+      <vxe-column field="brand" title="店铺品牌"></vxe-column>
+      <vxe-column title="操作" width="150">
+        <template #default="{ row }">
+          <vxe-button mode="text" icon="vxe-icon-edit" style="color: #3883fa; font-weight: 900" @click="editEvent(row)">编辑</vxe-button>
+          <vxe-button mode="text" icon="vxe-icon-delete" style="color: red; font-weight: 900" @click="removeEvent(row)">删除</vxe-button>
+        </template>
+      </vxe-column>
+    </vxe-table>
+    <vxe-pager
+      @page-change="account().getDataApi()"
+      background
+      v-model:current-page="pageValue.page"
+      v-model:page-size="pageValue.limit"
+      :total="pageValue.total"
+      :layouts="['PrevJump', 'PrevPage', 'JumpNumber', 'NextPage', 'NextJump', 'Sizes', 'FullJump', 'Total']"></vxe-pager>
+  </div>
 </template>
 
 <script setup>
-import { ref } from "vue";
+import { onMounted } from "vue";
+import account from "../store.js";
+const { tableData, pageValue, identity, form } = storeToRefs(account());
+
+onMounted(() => {
+  identity.value = "品牌管理员";
+  account().getDataApi();
+});
+
+function editEvent(row) {}
+function removeEvent(row) {}
 </script>
 
 <style lang="scss" scoped></style>
---
+++ src/pages/account/store.js
@@ -0,0 +1,35 @@
+import { defineStore } from "pinia";
+export default defineStore("account", {
+  state() {
+    return {
+      tableData: [], //数据
+      form: {}, //回显信息
+      identity:"",//身份
+      //分页
+      pageValue: {
+        page: 1,
+        limit: 10,
+        total: 0,
+        loading: false,
+      },
+    };
+  },
+  actions: {
+    async getDataApi() {
+      this.pageValue.loading = true;
+      const { data } = await $axios.get("/account/getAccount", {
+        params: {
+          limit: this.pageValue.limit,
+          page: this.pageValue.page,
+          identity:this.identity,
+        },
+      });
+      if (data.total || data.total == 0) {
+        this.pageValue.total = data.total;
+      }
+      this.tableData = data.data;
+      this.pageValue.loading = false;
+    },
+  },
+  persist: false,
+});
